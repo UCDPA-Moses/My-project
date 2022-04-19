b27f33d7c94105030f4018aeed5f47137ce3cb88
UCDPA-Moses <103839838+UCDPA-Moses@users.noreply.github.com>
2022-04-19 04:43:07 +0100

    Update my_project.py

--- my_project.py
+++ my_project.py
@@ -2,7 +2,7 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing and preparation
 import matplotlib.pyplot as plt # Visualization
-import seaborn as sns # Visualization on top of matplotlib
+import seaborn as sns # Advanced visualization
 
 #import data
 df = pd.read_csv("https://raw.githubusercontent.com/UCDPA-Moses/UCDPA_Moses/main/Car_sales.csv")
