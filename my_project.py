11aeb915394b797f3fa32fe443fc2faebb2639d1
UCDPA-Moses <103839838+UCDPA-Moses@users.noreply.github.com>
2022-04-19 04:42:40 +0100

    Update my_project.py

--- my_project.py
+++ my_project.py
@@ -1,6 +1,6 @@
 # importing necessary libraries
 import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import pandas as pd # data processing and preparation
 import matplotlib.pyplot as plt # Visualization
 import seaborn as sns # Visualization on top of matplotlib
 
