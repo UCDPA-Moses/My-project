c98dd86b06a778e865ca178c8f8dfb6fae05e9ad
UCDPA-Moses <103839838+UCDPA-Moses@users.noreply.github.com>
2022-04-19 14:56:48 +0100

    Boolean and control logic

---
+++ python_basics.py
@@ -0,0 +1,10 @@
+#This page is just to highlight and show case my knowledge of Boolean logic, control flow and loops in python
+
+num=-3
+
+if num==0:
+    print(f"{num} is a Zero was entered!! ")
+elif num%2==0:
+    print(f"{num} is an Even number!! ")
+else:
+    print(f"{num} is an Odd number!! ")
